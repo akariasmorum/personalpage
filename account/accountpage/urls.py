1e5256658fbe0e8a5b7187d825410ab46c65c0f1
akariasmorum <orensoftwaredeveloper@gmail.com>
2018-10-21 13:37:47 +0500

    залил шаблоны

--- account/accountpage/urls.py
+++ account/accountpage/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.conf.urls import url, include
+import django.contrib.auth.urls
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+	url(r'', include(django.contrib.auth.urls)),
+	url('call', views.calldoc, name='call-doc'),
+	url('ehr', views.ehr, name='ehr'),
+	url('appointment', views.app, name='appointment'),
+	url('schedule', views.schedule, name='schedule'),
+	url('calendar', views.calendar, name='calendar'),	
 ]
--- account/accountpage/views.py
+++ account/accountpage/views.py
@@ -1,8 +1,52 @@
 from django.shortcuts import render
+import http.client as hc
+import json
 
 # Create your views here.
-from django.http import HttpResponse
+def login(request):
+	if 'nick' in request.POST:		
+		request.session['user'] = request.POST['nick']
+		request.session.save()
+		user = {'nickname': request.session['user']}
+		#return render(request, 'rendered.html', context={'title':'welcome', 'user':user})
+	else:
+		return render(request, 'login.html', context = {'title':'Аутентификация'})
+def schedule(request):	
+	return render(request, 'schedule.html', context={'title': 'Расписание', 'nbar': 'schedule'})		
+def calldoc(request):
+	return render(request, 'calldoc.html', context={'title': 'Вызов врача на дом', 'nbar': 'call-doc'})
+def ehr(request):
+	return render(request, 'ehr.html', context={'title': 'Электронная медицинская карта', 'nbar': 'ehr'})
+def app(request):
+	return render(request, 'appointment.html', context={'title': 'Запись на прием', 'nbar': 'app'})			
+def calendar(request):
+	con = hc.HTTPConnection('ibus.dgkb.lan', 80) 
+	headers = { 
+	'Authorization': 'Basic cm9vdDpyb290', 
+	'Host': 'localhost:5000', 
+	'Content-Type': 'application/json' 
+	} 
 
+	parametr = str(json.dumps({'snils': '123123123'})) 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	
+
+	body = json.dumps({ 
+	'name': 'CalendarList', 
+	'params': {'request': parametr} 
+	}) 
+
+	con.request('POST', '/api/executescript', body=body, headers=headers) 
+
+	c = con.getresponse().read().decode() 
+	a = json.loads(c)
+
+	x= json.loads(a['output']['CalendarList'])
+
+	data = []
+	for dates in x:
+		first = [dates['NRU_GROUP'], dates['datedoc']]
+		data.append(first)
+		
+
+	return render(request, 'calendar.html', context = {'title': 'Календарь пациента', 'nbar': 'calendar', 'data':  data})
