4674f04ef5a5d6950b7d342cdf131992bd4c7d10
akariasmorum <akariasmorum@gmail.com>
2018-11-05 17:15:44 +0500

    Добавил верстку

--- account/accountpage/forms.py
+++ account/accountpage/forms.py
@@ -6,13 +6,13 @@ from django.views import generic
 from .models import PatientUser 
 
 class Message(forms.Form):
-	snils = forms.CharField(label='СНИЛС', max_length=14)
-	id_doc_site = forms.CharField(label='ID вставки', max_length=5)
-	recipient = forms.CharField(label='Кому обращение', max_length=20)
-	subject = forms.CharField(label='Тема сообщения', max_length=20)
-	message = forms.CharField(label='Сообщение', max_length=200)
-	phone = forms.CharField(label='Телефон', max_length=16)
-	email = forms.CharField(label='E-Mail', max_length=20)
+	snils = forms.CharField(widget =forms.TextInput(attrs={'class': 'form-control'}),label='СНИЛС', max_length=14)
+	id_doc_site = forms.CharField(widget =forms.TextInput(attrs={'class': 'form-control'}),label='ID вставки', max_length=5)
+	recipient = forms.CharField(widget =forms.TextInput(attrs={'class': 'form-control'}),label='Кому обращение', max_length=20)
+	subject = forms.CharField(widget =forms.TextInput(attrs={'class': 'form-control'}),label='Тема сообщения', max_length=20)
+	message = forms.CharField(widget =forms.Textarea(attrs={'class': 'form-control'}),label='Сообщение', max_length=200)
+	phone = forms.CharField(widget =forms.TextInput(attrs={'class': 'form-control'}),label='Телефон', max_length=16)
+	email = forms.CharField(widget =forms.TextInput(attrs={'class': 'form-control'}),label='E-Mail', max_length=20)
 
 
 class SignUpForm(UserCreationForm):
