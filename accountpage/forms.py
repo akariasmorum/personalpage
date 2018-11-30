45eb512a4b8e1cb1c561d54335fb7393cebbd6d0
akariasmorum <orensoftwaredeveloper@gmail.com>
2018-11-30 12:08:08 +0500

    убран padding в тасках в расписании

--- accountpage/forms.py
+++ accountpage/forms.py
@@ -83,7 +83,7 @@ class CallDoctorForm(ModelForm):
 	house        = forms.CharField    (widget = forms.TextInput(attrs={'class': 'form-control','id':'house'     }),label='Дом', max_length=10)
 	room         = forms.CharField    (widget = forms.TextInput(attrs={'class': 'form-control','id':'room'      }),label='Квартира', max_length=10, required = False)
 
-	add_inform   = forms.CharField    (widget = forms.Textarea (attrs={'class': 'form-control','id':'add_inform'}),label='Дополнительная информация', max_length=1000, required = False)
+	add_inform   = forms.CharField    (widget = forms.Textarea (attrs={'class': 'form-control addition','id':'add_inform'}),label='Дополнительная информация', max_length=1000, required = False)
 	patient      = forms.CharField    (widget = forms.Select   (attrs={'class': 'form-control','id':'pacient'   }),label='Пациент')
 
 	'''def __init__(self, *args, **kwargs):
