01aeee8660d68e7d959a1315550417f7d02885e2
Michael <akariasmorum@gmail.com>
2019-01-24 14:37:36 +0500

    Исправлен вывод даты рождения

--- accountpage/middleware.py
+++ accountpage/middleware.py
@@ -7,6 +7,9 @@ class Modify(object):
             if str(request.user) != 'AnonymousUser':
                 from .views import request_user_children
                 request.session['pacients'] = request_user_children(request.user.snils)
+                #for q in request.session['pacients']:
+                    #q['dater'] = '{0}.{1}.{2}'.format(q['dater'][8:10], q['dater'][5:7], q['dater'][:4])
+
 
         response = self.get_response(request)
         return response
