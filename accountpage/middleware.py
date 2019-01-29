e2769496e2834e76b91f4c06b050cbcd50545d5a
akariasmorum <orensoftwaredeveloper@gmail.com>
2019-01-29 14:24:13 +0500

    Исправлена ошибка с сессией

--- accountpage/middleware.py
+++ accountpage/middleware.py
@@ -5,8 +5,8 @@ class Modify(object):
     def __call__(self, request):
         if not request.session.get('pacients'):
             if str(request.user) != 'AnonymousUser':
-                from .views import request_user_children
-                request.session['pacients'] = request_user_children(request.user.snils)
+                from .views_ibus_connector import request_user_children
+                request.session['pacients'] = request_user_children(request, request.user.snils)
                 #for q in request.session['pacients']:
                     #q['dater'] = '{0}.{1}.{2}'.format(q['dater'][8:10], q['dater'][5:7], q['dater'][:4])
 
